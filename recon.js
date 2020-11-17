c54a24abf115a55d1a074f5f824d155b0b3ac8ca
Agah Karakuzu <agahkarakuzu@gmail.com>
2020-11-16 22:37:37 -0500

    another option

--- recon.js
+++ recon.js
@@ -47,6 +47,7 @@ function reconBlock(input) {
   //this.sort = RthReconSort();
   //this.sort.setIndexKeys(["acquisition.index"]);
   this.sort.setInput(input);
+  this.sort.setUseSliceEncodeKey(false);
   this.sort.setSwapSePe(true);
   this.sort.observeKeys(["acquisition.slice", "acquisition.index"]);
   this.sort.observedKeysChanged.connect(function(keys){
